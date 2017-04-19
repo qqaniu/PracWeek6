4258ea821c6d6383cbf00fb13f6b9a653455e38a
QANI QANIU <qqaniu@10-150-45-77.dhcp-m.deakin.edu.au>
2017-04-20 08:10:57 +1000

    test

---
+++ Practical6/Practical6/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Practical6
+//
+//  Created by QANI QANIU on 20/04/2017.
+//  Copyright © 2017 QANI QANIU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
